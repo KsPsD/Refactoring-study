8c6093cb886b9f3fe597a6d845597cca5721c186
KsPsD <ui9621@naver.com>
2022-01-30 01:41:45 +0900

    example code

---
+++ plays.js
@@ -0,0 +1,6 @@
+export default{
+    "hamlet":{"name": "Halmet", "type":"tragedy"},
+    "as-like":{"name": "As You Like It", "type":"comedy"},
+    "othello":{"name":"Othello", "type":"tragedy"}
+}
+
