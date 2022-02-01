bb6522d1f4fdf2135c814c77b7cb51c99a63d762
KsPsD <ui9621@naver.com>
2022-02-01 17:00:53 +0900

    extract amount function

--- show.js
+++ show.js
@@ -2,17 +2,10 @@ import plays from "./plays.js"
 import invoices from "./invoices.js" 
 
 
-function statement(invoice, plays){
-    let totalAmount =0
-    let volumeCredits = 0
-    let result =`청구 내역 (고객명: ${invoice.customer})\n`;
-    const format= new Intl.NumberFormat('en-US', {style: "currency", currency: "USD", minimumFractionDigits: 2}).format
 
-    for(let perf of invoice.performances){
-        const play = plays[perf.playID]
-        let thisAmount = 0;
-
-       switch(play.type){
+function amountFor(perf,play){
+    let thisAmount = 0;
+    switch(play.type){
         case "tragedy":
                thisAmount = 40000;
                if(perf.audience>30){
@@ -29,11 +22,25 @@ function statement(invoice, plays){
         default:
             throw new Error(`알 수 없는 장르: ${play.type}`)
        }
+       return thisAmount
+}
+
+function statement(invoice, plays){
+    let totalAmount =0
+    let volumeCredits = 0
+    let result =`청구 내역 (고객명: ${invoice.customer})\n`;
+    const format= new Intl.NumberFormat('en-US', {style: "currency", currency: "USD", minimumFractionDigits: 2}).format
+
+    for(let perf of invoice.performances){
+        const play = plays[perf.playID]
+        let thisAmount = 0;
+
+       let thisAmount=amountFor(perf,play)
        volumeCredits +=Math.max(perf.audience -30 ,0);
        if("comedy" ===play.type) volumeCredits +=Math.floor(perf.audience / 5);
 
 
-       result+=` ${play.name}: ${format(thisAmount/100)} (${perf.audience} 석) \n`
+       result+=`${play.name}: ${format(thisAmount/100)} (${perf.audience} 석) \n`
        totalAmount +=thisAmount
     }
     result+=`총액: ${format(totalAmount/100)}\n`
