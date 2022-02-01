11d93b29fc21d894c23d82a8a0e831be10fab210
KsPsD <ui9621@naver.com>
2022-02-01 23:01:54 +0900

    replace temp with query

--- show.js
+++ show.js
@@ -1,28 +1,5 @@
 import plays from "./plays.js" 
 import invoices from "./invoices.js" 
-
-function amountFor(perf,play){
-    let result = 0;
-    switch(play.type){
-        case "tragedy":
-               thisAmount = 40000;
-               if(perf.audience>30){
-                result+=1000 * (perf.audience -30);
-               }
-               break
-        case "comedy":
-            thisAmount = 30000;
-            if(perf.audience>20){
-                result +=10000 +500 * (perf.audience -20);
-            }
-            result +=300 * perf.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${play.type}`)
-       }
-       return result
-}
-
 function statement(invoice, plays){
     let totalAmount =0
     let volumeCredits = 0
@@ -30,7 +7,7 @@ function statement(invoice, plays){
     const format= new Intl.NumberFormat('en-US', {style: "currency", currency: "USD", minimumFractionDigits: 2}).format
 
     for(let perf of invoice.performances){
-        const play = plays[perf.playID]
+        const play = playFor(perf)
         let thisAmount = 0;
 
        let thisAmount=amountFor(perf,play)
@@ -46,6 +23,32 @@ function statement(invoice, plays){
     return result;
 
 }
+function playFor(aPerformance){
+    return plays[aPerformance.playID]
+}
+function amountFor(aPerformance,play){
+    let result = 0;
+    switch(play.type){
+        case "tragedy":
+               thisAmount = 40000;
+               if(aPerformance.audience>30){
+                result+=1000 * (aPerformance.audience -30);
+               }
+               break
+        case "comedy":
+            thisAmount = 30000;
+            if(aPerformance.audience>20){
+                result +=10000 +500 * (aPerformance.audience -20);
+            }
+            result +=300 * aPerformance.audience;
+            break;
+        default:
+            throw new Error(`알 수 없는 장르: ${play.type}`)
+       }
+       return result
+}
+
+
 
 
 console.log(statement(invoices[0],plays))
