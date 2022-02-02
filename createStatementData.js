2c6f9876c41a2fd32f8c9093634308219635ac60
KsPsD <ui9621@naver.com>
2022-02-02 16:07:37 +0900

    separate module statement data

--- createStatementData.js
+++ createStatementData.js
@@ -1,14 +1,11 @@
-import plays from "./plays.js" 
-import invoices from "./invoices.js" 
-function statement(invoice, plays){
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+export default function createStatementData(invoice, plays){
+    const result = {};
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
 
-    return renderPlainText(statementData, plays)
-    
+    return result
 
     function enrichPerformance(aPerformance){
         const result = Object.assign({}, aPerformance); //얕은 복사 수행
@@ -61,36 +58,8 @@ function statement(invoice, plays){
     function totalVolumeCredits(data){
        return data.performances.reduce((total, p) => total +p.volumeCredits,0)
     }
-    
 
-    
-    
+
+
 
 }
-
-function renderPlainText(data, plays){
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
-    for(let perf of data.performances){
-        result+=`${perf.play.name}: ${usd(perf.amount)} (${perf.audience} 석) \n`
-    }
-    result+=`총액: ${usd(data.totalAmount)}\n`
-    result+=`적립 포인트: ${data.totalVolumeCredits}점 \n`
-    return result;
-
-   
-    
-    function usd(aNumber){
-        return new Intl.NumberFormat('en-US', {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber/100)
-    }
-    
-   
-   
-    
-
-}
-
-
-
-
-
-console.log(statement(invoices[0],plays))
